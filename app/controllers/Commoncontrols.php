0b9f08a2a80664c65c423635d848a3d4d49a5956
ANEASER <anudaattanayake@gmail.com>
2023-09-24 16:22:50 +0530

    login completed

--- app/controllers/Commoncontrols.php
+++ app/controllers/Commoncontrols.php
@@ -12,5 +12,52 @@ class CommonControls extends Controller {
     function loadRegisterView() {
         $this->view("common/register");
     }
+
+    function login() {
+        $errors = array();
+    
+        if (count($_POST) > 0) {
+            $systemuser = new Systemuser();
+    
+            if ($row = $systemuser->where("UserName", $_POST["username"])) {
+                if (is_array($row) && count($row) > 0) {
+                    
+                    $user = $row[0];
+                    
+                    if (isset($user->Role)) {
+                        $role = $user->Role;
+                        
+                        if ($_POST["password"] == $user->Password) {
+                            var_dump($user);
+                            Auth::authenticate($user);
+                            
+                            if ($role == 'admin') {
+                                $this->redirect("../admincontrols");
+                            } elseif ($role == 'storemanager') {
+                                $this->redirect("../storemanagercontrols");
+                            } elseif ($role == 'receptionist') {
+                                $this->redirect("../Recieptioncontrols");
+                            }elseif ($role == 'billingclerk') {
+                                $this->redirect("../billingcontrols");
+                            }else {
+                                $this->redirect("../pmcontrols");
+                            }
+                        } else {
+                            $errors[] = "Invalid password";
+                        }
+                    } else {
+                        $this->redirect("../customercontrols");
+                    }
+                    } else {
+                        $errors[] = "User data does not contain a password";
+                    }
+                } else {
+                    $errors[] = "User not found";
+                }
+            } else {
+                $errors[] = "User not found";
+            }
+    }
 }
+    
 ?>
--- app/views/common/login.php
+++ app/views/common/login.php
@@ -7,14 +7,15 @@
 </head>
 <body>
     <h1>Login</h1>
-    <form action="#" method="POST">
+    <form method="POST" action="login">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
         <br>
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
         <br>
-        <input type="submit" value="Login">
+
+        <button type="submit">Login</button>
     </form>
 </body>
 </html>
