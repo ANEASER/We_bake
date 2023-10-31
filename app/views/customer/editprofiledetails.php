ed5e82833a6066d8dcb81a22bd0d085f2640f9ee
Venuri-Abeysekara <2021is003@stu.ucsc.cmb.ac.lk>
2023-10-31 21:43:31 +0530

    Updated customer pages

--- app/views/customer/editprofiledetails.php
+++ app/views/customer/editprofiledetails.php
@@ -18,33 +18,33 @@
             <div class="sub-container">
             <form action="dashboard.php">
                 <div class="formcontent" >
-                    <div>
+                    <div class="form-group">
                         <h4>Customer ID:</h4>
                         <label>
                             <input name="CID" type="text" id="CID" value="<?php echo $row['CustomerID'];?>" required />
                         </label>
                     </div>
-                    <div>
+                    <div class="form-group">
                         <h4>Customer Name:</h4>
                         <label>
                             <input name="customername" type="text" id="customername" value="<?php echo $row['CustomerName'];?>" required />
                         </label>
                     </div>
-                    <div>
+                    <div class="form-group">
                         <h4>Address:</h4>
                         <label>
                             <input name="customeraddress" type="text" id="customeraddress" value="<?php echo $row['CustomerAddress'];?>" required />
                         </label>
                     </div>
 
-                    <div>
+                    <div class="form-group">
                         <h4>City:</h4>
                         <label>
                             <input name="customercity" type="text" id="customercity" value="<?php echo $row['CustomerCity'];?>" required />
                         </label>
                     </div>
 
-                    <div>
+                    <div class="form-group">
                         <h4>Email Address:</h4>
                         <label>
                             <input name="customeremail" type="email" id="customeremail" value="<?php echo $row['CustomerEmail'];?>" required />
@@ -55,7 +55,7 @@
                         <label>
                             <input name="customermobile" type="text" id="customermobile" value="<?php echo $row['CustomerMobile'];?>" required />
                         </label>
-                    </div>
+                    </div class="form-group">
                     <div>
                     <input type="submit" name="submit" id="submit" value="Update Profile" />
                     </div>
--- app/views/customer/sidebar.php
+++ app/views/customer/sidebar.php
@@ -8,7 +8,7 @@
         <ul>
             <li><a href="customerdash.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
-            <li><a href="order.php">Order</a></li>
+            <li><a href="placeorder.php">Place Order</a></li>
             <li><a href="purchasehistory.php">Purchase History</a></li>
             <li><a href="makeinquiry.php">Make Inquiry</a>
         </ul>
