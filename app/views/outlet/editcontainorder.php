ed378a4a74e8f71650537ec6f006fc0816506ab3
Kumudu2001 <2021is105@stu.ucsc.cmb.ac.lk>
2023-11-01 10:55:25 +0530

    outletui

---
+++ app/views/outlet/editcontainorder.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outlet Manager</title>
+    <link rel="stylesheet" href="outletmanager.css">
+</head>
+<body>
+    <?php
+        include "omnavbar.php";
+    ?>
+    div class="content">
+ <h1>Purchase History</h1>
+        <div>
+        <table>
+        <tr>
+        <center>    <th>header 1</th> </center>
+        
+           </tr>
+</dev>
+    
+</body>
+</html>
---
+++ app/views/outlet/placeorder.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Outlet Manager</title>
+    <link rel="stylesheet" type="text/css" href="http://localhost/we_bake/app/views/outlet/outletmanager.css">
+</head>
+<body>
+    <?php
+        include "omnavbar.php";
+    ?>
+         
+    <div class="content">
+ <h1>Purchase History</h1>
+        <div>
+        <table>
+            <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>2</td>
+            </tr>
+        
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+       
+        </table>
+
+        </div>        
+    </div>
+      <script src="script.js"></script>
+     
+   
+</body>
+</html>
+</body>
+</html>
+
+
+
+
+
