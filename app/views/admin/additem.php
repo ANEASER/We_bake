fee52a21dbe8276f5c9957da25221fbaa3e574ab
ANEASER <anudaattanayake@gmail.com>
2023-11-01 11:51:59 +0530

    admin UI completed

--- app/views/admin/additem.php
+++ app/views/admin/additem.php
@@ -7,28 +7,39 @@
     <title>Add Product Item</title>
 </head>
 <body style="font-weight: 800">
-    <h1 class="header">Add Product Item</h1>
+    
     <div class="container">
         <div class="sub-container" style="width: 20%;">
+            <h1 >Add Product Item</h1>
             <button class="navbutton" onclick="back()">Back</button>
         </div>
 
-        <div class="sub-container" style="width: 80%;" >
+        <div class="container" style="width: 80%;" >
             <form class="form-group" method="POST" style="padding: 3%;" action="addproductitem">
 
+            <div class="form-group">
                 <label for="itemname">Item Name:</label><br>
                 <input type="text" id="itemname" name="itemname" required><br><br>
+            </div>
 
+            <div class="form-group">
                 <label for="retailprice">Retail Price:</label><br>
                 <input type="number" id="retailprice" name="retailprice" required><br><br>
+            </div>
 
+            <div class="form-group">
                 <label for="stockprice">Stock Price:</label><br>
                 <input type="number" id="stockprice" name="stockprice" required><br><br>
+            </div>
 
+            <div class="form-group">
                 <label for="itemdescription">Item Description:</label><br>
                 <textarea id="itemdescription" name="itemdescription" rows="4" required></textarea><br><br>
-
+            </div>
+            
+            <div class="button-container">     
                 <input class="button"  type="submit" value="Submit">
+            </div>
             </form>
         </div>
     </div>
--- app/views/admin/addvehicle.php
+++ app/views/admin/addvehicle.php
@@ -3,40 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="http://localhost/we_bake/public/css/main.css">
+    <link rel="stylesheet" type="text/css" href="http://localhost/we_bake/app/views/admin/adminstyle.css">
     <title>Add Delivery Vehicle</title>
 </head>
 <body>
-    <h1 class="header">Add Delivery Vehicle</h1>
+    
     <div class="container">
         <div class="sub-container navbar">
+            <h1 class="header">Add Delivery Vehicle</h1>
             <button class="navbutton" onclick="back()">Back</button>
         </div>
 
         <div class="sub-container content">
-            <form class="form-group" method="POST" style="padding: 3%;">
+            <form method="POST" style="padding: 3%;">
+                <div class="form-group"> 
                 <label for="registrationnumber">Registration Number:</label><br>
                 <input type="text" id="registrationnumber" name="registrationnumber" required><br><br>
-
+                </div>
+                <div class="form-group"> 
                 <label for="type">Type:</label><br>
                 <input type="text" id="type" name="type" required><br><br>
-
+                </div>
+                <div class="form-group"> 
                 <label for="capacity">Capacity:</label><br>
                 <input type="number" id="capacity" name="capacity" required><br><br>
-
+                </div>
+                <div class="form-group"> 
                 <label for="vehicleno">Vehicle No:</label><br>
                 <input type="text" id="vehicleno" name="vehicleno" required><br><br>
-
+                </div>
+                <div class="form-group"> 
                 <label for="chassinumber">Chassis Number:</label><br>
                 <input type="text" id="chassinumber" name="chassinumber" required><br><br>
-
+                </div>
+                <div class="form-group"> 
                 <label for="enginenumber">Engine Number:</label><br>
                 <input type="text" id="enginenumber" name="enginenumber" required><br><br>
-
+                </div>
+                <div class="form-group"> 
                 <label for="modelname">Model Name:</label><br>
                 <input type="text" id="modelname" name="modelname" required><br><br>
-
-                <input class="button" type="submit" value="Submit">
+                </div>
+                <div class="button-container"> 
+                <input class="formbutton" type="submit" value="Submit">
+                </div>
             </form>
         </div>
     </div>
