04950f921d4ef47bd28742a91eaa2ea06dc89019
Kumudu2001 <2021is105@stu.ucsc.cmb.ac.lk>
2023-11-02 15:11:20 +0530

    oultetnew4

--- app/views/outlet/editprofile.php
+++ app/views/outlet/editprofile.php
@@ -3,11 +3,14 @@
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Customer Profile</title>
+    <title>Edit Outlet Manager Profile</title>
     <link rel="stylesheet" href="http://localhost/we_bake/app/views/outlet/outletmanager.css">
 
 </head>
 <body>
+<!--<div class="header">
+        <h1>Edit Your Profile</h1>
+    </div>-->
     <div class="container">
 
         <?php
@@ -16,19 +19,21 @@
     
     <div class="sub-container" >
         <div>
+        <h1 style="text-align:center;">Edit Your Profile</h1>
+
             <div>
-                <button onclick="editprofiledetails()" class="buttonedit">Edit Profile Details</button>
+                <button onclick="editoutletmanagerprofiledetails()" class="buttonedit">Edit Outlet Manager Profile Details</button>
             </div>
             <div>
-                <button onclick="changepassword()" class="buttonedit">Change Password</button>
+                <button onclick="outletmanagerchangepassword()" class="buttonedit"> Outlet Manager Change Password</button>
             </div>
     </div>
     <script>
-    function editprofiledetails(){
-            window.location.href = "outletmanagercontrols/editprofiledetails";
+  function editprofiledetails(){
+    window.location.href = "http://localhost/we_bake/public/outletmanagercontrols/editoutletmanagerprofiledetails";
 }
     function changepassword(){
-            window.location.href = "outletmanagercontrols/changepassword";
+        window.location.href = "http://localhost/we_bake/public/outletmanagercontrols/outletmanagerchangepassword";
 }
 </script>
 </body>
