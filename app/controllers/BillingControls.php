7b68fdf48132e362367372603d543916a8916299
aneaser <anudaattanayake@gmail.com>
2024-02-04 10:43:11 +0530

    billing clerk authentication

--- app/controllers/BillingControls.php
+++ app/controllers/BillingControls.php
@@ -15,6 +15,16 @@ class BillingControls extends Controller {
 
     // order handle
     function processOrder($orderid,$paymentstatus) {
+
+        if(!Auth::loggedIn()){
+            $this->redirect(BASE_URL."CommonControls/loadLoginView");
+        }
+
+        if (!Auth::isBillingClerk()) {
+            $this->redirect(BASE_URL."CommonControls/loadLoginView");
+        }
+        
+
         $productorder = new ProductOrder();
         $order = $productorder->where("orderid",$orderid);
         $total = $order[0]->total;
@@ -36,22 +46,29 @@ class BillingControls extends Controller {
                 $imageData = file_get_contents($imageTmpName);
                 $base64Image = base64_encode($imageData);
 
-               
                 $paymentproofs->insertImage($initialorfinal, $orderid, $base64Image);
                 $productorder->update($orderid,"orderid",["paymentstatus" => $initialorfinal, "paid_amount" => $amount]);
                 $this->redirect(BASE_URL."BillingControls/index");
-
-                
     
             } else {
                 echo "Error uploading image.";
-                $this->redirect(BASE_URL."BillingControls/index");
+                echo $this->view("billingclerk/uploadproof");
             }
             
 
     }
 
     function viewProfile(){
+
+        if(!Auth::loggedIn()){
+            $this->redirect(BASE_URL."CommonControls/loadLoginView");
+        }
+
+        if (!Auth::isBillingClerk()) {
+            $this->redirect(BASE_URL."CommonControls/loadLoginView");
+        }
+        
+        
         echo $this->view("billingclerk/profile");
     }
     
