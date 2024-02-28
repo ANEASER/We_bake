798b3b807f6f4dc5c6fd9a2b18de7e9f562c06bb
pinsariesigera <2021is090@stu.ucsc.cmb.ac.lk>
2024-02-28 14:56:50 +0530

    Store Manager profile css

--- app/views/storemanager/profile.php
+++ app/views/storemanager/profile.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/main.css">
+    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/userprofile.css">
     <title>Store Manager_Profile</title>
 </head>
 <body>
@@ -10,7 +12,20 @@
         include "smnavbar.php";
     ?>
 
-    <h1>Store Manager_Profile</h1>
+    <div class="profile-container">
+        <div class="profile-picture">
+            <img src="<?php echo BASE_URL ?>media/uploads/Content/user.png" alt="Profile Picture">
+        </div>
+        <div class="profile-details">
+            <h2>User Profile</h2>
+            <p><strong>Name:</strong> John Doe</p>
+            <p><strong>Email:</strong> johndoe@example.com</p>
+            <p><strong>Phone:</strong> 123-456-7890</p>
+            <p><strong>Address:</strong> 123 Main Street, City, Country</p>
+        </div>
+    </div>
+
+    
     
 </body>
 </html>
--- app/views/storemanager/stocks.php
+++ app/views/storemanager/stocks.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/main.css">
     <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/buttons.css">
     <title>Store Manager_Stocks</title>
 </head>
