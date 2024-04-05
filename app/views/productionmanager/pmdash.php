fb9f287d38b3caed3b4f655b5e9af022a4461ee1
Venuri Chamathka Abeysekara <venuriabeysekara@192.168.1.5>
2024-04-05 15:22:41 +0530

    other

--- app/views/productionmanager/pmdash.php
+++ app/views/productionmanager/pmdash.php
@@ -4,18 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Production Manager Dashboard</title>
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/tables.css">
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/buttons.css">
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/cart.css">
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/main.css">
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/navbar.css">
-   
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>media/css/tables.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>media/css/buttons.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>media/css/cart.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>media/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>media/css/navbar.css">
 </head>
 <body>
     <?php
         require('pmnavbar.php');
     ?>
     <h1>Production Manager Dashboard</h1>
-    <h1>hiii</h1>
+    
 </body>
 </html>
--- app/views/productionmanager/pmnavbar.php
+++ app/views/productionmanager/pmnavbar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel = "stylesheet" type = "text/css" href = "<?php echo BASE_URL; ?>media/css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>media/css/navbar.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <title></title>
@@ -19,12 +19,12 @@
         <div class="bar3"><i id="sidebar btn"></i></div>
     </label>
 
-    <?php 
-    echo '<img class="logo" src=".BASE_URL.'media/uploads/Content/logo.png" width="200">';
+    <?php
+            echo '<img class="logo" src="' . BASE_URL . 'media/uploads/Content/logo.png" width="200px">';
     ?>
 
     <ul>
-    <li><a class="navbutton" onclick="home(this)">Home </a></li>
+    <li><a class="navbutton" onclick="home(this)">Home</a></li>
     <li><a class="navbutton" onclick="loadVehicles(this)">Vehicles</a></li>
     <li><a class="navbutton" onclick="logout()">LogOut</a></li>
     </ul>
@@ -32,46 +32,44 @@
     </nav>
 
     <script>
+    var BASE_URL = "<?php echo BASE_URL; ?>";
 
-    var BASE_URL="<?php echo BASE_URL; ?>";
-
-    var activeLink=sessionStorage.getlItem("activeLink");
-    if(activeLink!=null){
-        var linkElement= document.querySelector('a[onclick="'+activeLink+'"]');
-        if(linkElement!=null){
+    var activeLink = sessionStorage.getItem("activeLink");
+    if(activeLink != null){
+        var linkElement = document.querySelector('a[onclick="'+activeLink+'"]');
+        if(linkElement != null){
             linkElement.classList.add('active');
         }
     }
     else{
-        var homeLink=document.querySelector('a[onclick="home(this)"]');
-        if(homeLink!=null){
+        var homeLink = document.querySelector('a[onclick="home(this)"]');
+        if(homeLink != null){
             homeLink.classList.add('active');
-            sessionStorage.setlItem('activeLink',homeLink.getAttribute('onclick'));
+            sessionStorage.setItem('activeLink', homeLink.getAttribute('onclick'));
         }
     }
     function changeActive(link){
-        vae links=document.query.SelectorAll('body nav ul a');
+        var links = document.querySelectorAll('body nav ul a');
         links.forEach(function(el){
             el.classList.remove('active');
         });
         link.classList.add('active');
-        sessionStorage.setlItem('activeLink',link.getAttribute('onclick'));
+        sessionStorage.setItem('activeLink', link.getAttribute('onclick'));
     }
 
     function home(link){
         changeActive(link);
-        window.location.href= BASE_URL+"PmControls/index";
+        window.location.href = BASE_URL + "PmControls/index";
     }
 
-    function loadvehicle(link){
+    function loadVehiclesView(link){
         changeActive(link);
-        window.loaction.href= BASE_URL + "PmComtrols/loadVehiclesView";
+        window.location.href = BASE_URL + "PmControls/loadVehiclesView";
     }
 
     function logout(){
-        window.location.href= BASE_URL + "CommonControls/logout";
+        window.location.href = BASE_URL + "CommonControls/logout";
     }
-
     </script>
 
 </body>
