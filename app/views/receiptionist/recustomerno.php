22d60a3c8922ce144740559915450b85aeae60ac
Kumudu2001 <2021is105@stu.ucsc.cmb.ac.lk>
2024-04-10 10:49:52 +0530

    today

--- app/views/receiptionist/recustomerno.php
+++ app/views/receiptionist/recustomerno.php
@@ -12,17 +12,70 @@
     <title>Place order</title>
 </head>
   
-    
-
 <body>
     <?php
         include "recnavbar.php";
     ?>
-    <br><br>
     
-   
+<br> <br>
+       <div class = "form-container">
+        <form class="form" action="<?php echo BASE_URL; ?>RecieptionControls/submitorder" method="post">
 
+        <?php
+        if (isset($customerfound)) {
+            echo "<h3>Customer Found</h3>";
+            echo "<h4>Customer Name: " . $customerfound[0]->Name . "</h4>";
+            echo "<h4>Customer Email: " . $customerfound[0]->Email . "</h4>";
+            echo "<h4>Customer Phone: " . $customerfound[0]->contactNo . "</h4>";
+            echo '
+                    <input type="hidden" id="customername" name="name" value="' . $customerfound[0]->Name . '" required><br>
+                    <input type="hidden" id="customeremail" name="email" value="' . $customerfound[0]->Email . '" required><br>
+                    <input type="hidden" id="customerphone" name="phone" value="' . $customerfound[0]->contactNo . '" required><br>
 
+            <div class="form-group">
+                    <label for="orderdate">Order Date:</label>
+                    <input type="date" id="orderdate" name="orderdate" required><br>
+            </div>
+
+                    <label for="deliver_address">Delivery Address:</label>
+                    <input type="text" id="deliver_address" name="deliver_address" required><br>
+                    
+                    <label for="deliverystatus">Delivery/Pickup:</label>
+
+                    <select id="deliverystatus" name="deliverystatus" required>
+                    <option value="delivery">Delivery</option>
+                    <option value="pickup">Pickup</option>
+                    </select><br>';
+
+            echo "<button type='button' onclick='newcustomer()'>New Customer</button>";
+        } else {
+            echo '<label for="customername">Customer Name:</label>
+                <input type="text" id="customername" name="name" required><br>
+
+                <label for="customeremail">Customer Email:</label>
+                <input type="email" id="customeremail" name="email" required><br>
+
+                <label for="customerphone">Customer Phone:</label>
+                <input type="tel" id="customerphone" name="phone" required><br>
+
+                <label for="orderdate">Order Date:</label>
+                <input type="date" id="orderdate" name="orderdate" required><br>
+
+                <label for="deliver_address">Delivery Address:</label>
+                <input type="text" id="deliver_address" name="deliver_address" required><br>
+                
+                <label for="deliverystatus">Delivery/Pickup:</label>
+
+                <select id="deliverystatus" name="deliverystatus" required>
+                <option value="delivery">Delivery</option>
+                <option value="pickup">Pickup</option>
+                </select><br>';
+            }
+            ?>
+            
+        <br>
+        <input type="submit" value="Submit">
+    </form>
 
     <script>
         var BASE_URL = "<?php echo BASE_URL; ?>";
@@ -35,6 +88,7 @@
             window.location.href = BASE_URL + "RecieptionControls/moredetails/" + unique_id;
         }
     </script>
+    
 </body>
 </html>
 
