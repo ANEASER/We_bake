b3e5579dc04cebec0d3efec7a39fa5e7cb14fc60
aneaser <anudaattanayake@gmail.com>
2024-04-19 23:57:03 +0530

    sms stuff worked key removed

--- app/core/config.php
+++ app/core/config.php
@@ -3,8 +3,8 @@
     define("DB_USER","root");
     define("DB_PASSWORD","");
     define("DB_NAME","we bake");
-    define("SMS_API_KEY","a2d55f833230728591568388d93a3300-24a97d47-db1a-409e-856c-31f5af5a6b32");
-    define("SMS_API_URL","43l9e6.api.infobip.com");
+    define("SMS_API_KEY","none");
+    define("SMS_API_URL","none");
 
     define("BASE_URL","http://localhost/We_bake/public/");
     define("ASSETS_URL","http://localhost/We_bake/public/assets/")
