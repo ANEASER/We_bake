27ed05b4b2056a63095b43456adc150c2afec24a
venuri <2021is003@stu.ucsc.cmb.ac.lk>
2024-04-20 15:15:24 +0530

    edits

--- app/controllers/pmcontrols.php
+++ app/controllers/pmcontrols.php
@@ -269,7 +269,7 @@ class PmControls extends Controller
     }
 
     //Search Vehicle
-    fucntion searchVehicle(){
+    function searchVehicle(){
         if (!Auth::loggedIn()) {
             $this->redirect(BASE_URL . "CommonControls/loadLoginView");
         }
