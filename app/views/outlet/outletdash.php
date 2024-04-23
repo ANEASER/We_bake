b4240cf6feb755910f1ee95d4d7824f8b0cb4d3e
Kumudu2001 <2021is105@stu.ucsc.cmb.ac.lk>
2024-04-23 09:55:41 +0530

    om

--- app/views/outlet/outletdash.php
+++ app/views/outlet/outletdash.php
@@ -16,7 +16,37 @@
         include "omnavbar2.php";
     ?>
 
-    <h1> </h1>
+    <section class="content">
+            <section class="cart" style="padding : 2%;font-size: 1em;">
+                <h1 style="text-align:center">TODAY AVAILABILITIES</h1>
+                <?php
+                    echo "<br>";
+                    echo "<table>";
+                    echo "<tr>
+                            <th>Item Code </th>
+                            <th>Item Name</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>";
+
+                    foreach ($productorderlines as $productorderline) {
+                        
+                        $productitem = new ProductItem();
+                        $item = $productitem->where('Itemcode', $productorderline->Itemcode);
+                        
+                        echo "<tr>";
+                        echo "<td>" . $productorderline->Itemcode . "</td>";
+                        echo "<td>" . $item[0]->itemname. "</td>";
+                        echo "<td>" . $productorderline->quantity . "</td>";
+                        echo "<td>" . $productorderline->price . "</td>";
+                        echo "</tr>";
+                    }
+
+                    echo "</table>";
+
+                    ?>
+        </section>
+    </section>
 
     <script>
         var BASE_URL = "<?php echo BASE_URL; ?>";
